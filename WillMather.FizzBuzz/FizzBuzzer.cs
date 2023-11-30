bc966b967a2a5dd367f720119b456d4883b90bbc
will187 <williammather@sky.com>
2023-11-30 09:31:56 +0000

    Added New test to check for expected output. failing at this point

--- WillMather.FizzBuzz/FizzBuzzer.cs
+++ WillMather.FizzBuzz/FizzBuzzer.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WillMather.FizzBuzz
+namespace WillMather.FizzBuzz;
+
+public class FizzBuzzer
 {
-    public class FizzBuzzer
-    {
-      
-    }
+  public string RunFzzBuzz(int input)
+  {
+        return string.Empty;
+  }
 }
--- WillMather.FizzBuzzerTests/FizzBuzzerTests.cs
+++ WillMather.FizzBuzzerTests/FizzBuzzerTests.cs
@@ -1,7 +1,21 @@
-﻿namespace WillMather.FizzBuzzerTests
-{
-    public class FizzBuzzerTests
-    {
+﻿using NUnit.Framework;
+using WillMather.FizzBuzz;
 
+namespace WillMather.FizzBuzzerTests;
+
+[TestFixture]
+public class FizzBuzzerTests
+{
+    [TestCase(1, "1")]
+    public void FizzBuzzer_GivenIntInput_ReturnsExpectedString(int input, string expected)
+    {
+        // Arrange
+        FizzBuzzer fizzbuzzer = new FizzBuzzer();
+
+        // Act
+        string actual = fizzbuzzer.RunFzzBuzz(input);
+        
+        // Assert
+        Assert.Equals(expected, actual);
     }
 }
